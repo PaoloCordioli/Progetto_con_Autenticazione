46a934da11ffc4a86941c0f94c9b9a312daa725c
PaoloCordioli <p.cordioli@hotmail.it>
2020-03-23 14:28:21 +0100

    Cambiato nome di un bottone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ export default function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/login">New message</Nav.Link>
             <Nav.Link as={Link} to="/register">Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- frontend/src/Components/Messages.js
+++ frontend/src/Components/Messages.js
@@ -6,7 +6,7 @@ export default class Message extends Component {
     createMarker = (mes) => { // funzione che permette di creare un marker con un messaggio per ogni messaggio nello state
         return <Marker onMouseOver={(e) => { e.target.openPopup(); }}
             onMouseOut={(e) => { e.target.closePopup(); }}
-    key={Math.random()} position={[mes.lat, mes.lon]}><Popup><strong>{mes.content}</strong> <br></br> - {mes.username.charAt(0).toUpperCase() + mes.username.slice(1)}, {mes.date}</Popup></Marker>
+            key={Math.random()} position={[mes.lat, mes.lon]}><Popup><strong>{mes.content}</strong> <br></br> - {mes.username.charAt(0).toUpperCase() + mes.username.slice(1)}, {mes.date}</Popup></Marker>
     }
 
     render() {
